de146f24a19bcbb63872d1b24994912d1a1b8d86
unknown <blee14@L14-0518.babson.edu>
2015-11-16 21:27:36 -0500

    project update

---
+++ Term Project/index.js
@@ -0,0 +1,11 @@
+function linkFacebook() {
+	window.open("https://www.facebook.com/brendanl94");
+}
+
+function linkTwitter() {
+	window.open("https://twitter.com/CyphrSec")
+}
+
+function linkLinkedIn() {
+	window.open("https://www.linkedin.com/profile/view?id=AAIAABHiHWEBn5IH6HG239aemA3mjXVbpE254ms&trk=nav_responsive_tab_profile")
+}
